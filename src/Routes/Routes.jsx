98c76c1b97414582d68e99a0a72f906ec6839d97
MSabbirHossen <mshossen724@gmail.com>
2025-12-31 14:48:21 +0300

    React Router Set-up Completed.

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+import Root from '../components/pages/Root/Root';
+import ErrorPage from '../components/pages/ErrorPage/ErrorPage';
+import Home from '../components/pages/Home/Home';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <ErrorPage/>,
+    children: [
+        {
+            index: true,
+            path: "/",
+            Component: Home
+        }
+    ]
+  },
+]);
